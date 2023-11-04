7afbdc9200ba76c0f81f827695a5b79c1c424d04
Armaan A <armaanaggarwal@gmail.com>
2023-11-04 15:49:22 -0700

    init

---
+++ prettier.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  printWidth: 100,
+  plugins: [
+    require.resolve("prettier-plugin-astro"),
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
