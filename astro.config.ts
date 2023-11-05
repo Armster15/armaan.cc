5a21f3326dc3905ba5ba3a5ec723f07f1bd3e044
Armaan A <armaanaggarwal@gmail.com>
2023-11-04 19:35:59 -0700

    add actual blog content

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), expressiveCode(), mdx()],
 });
