85ad3f5129a1cf638fc97bc469d4ea7f9cede6b8
Armaan A <armaanaggarwal@gmail.com>
2023-11-04 20:31:22 -0700

    add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), expressiveCode(), mdx()],
+  site: "https://armaan.cc",
+  integrations: [
+    tailwind(),
+    expressiveCode(),
+    mdx(),
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
 });
