1019fd7206de3cd5115655ce174c44543f033740
Armaan A <armaanaggarwal@gmail.com>
2023-11-18 19:04:39 -0800

    make og image text bigger

--- src/lib/og-image/generate.tsx
+++ src/lib/og-image/generate.tsx
@@ -20,21 +20,21 @@ interface GenerateOgImage {
 export async function generateOgImage({ title, subtitle }: GenerateOgImage) {
   const markup = (
     <div tw="flex p-10 h-full w-full bg-white flex-col">
-      <header tw="flex items-center text-[36px] w-full">
-        <svg tw="rounded-full h-16 w-16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+      <header tw="flex items-center w-full">
+        <svg tw="rounded-full h-24 w-24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
           <path d="M 0 0 L 100 100 L 0 100" fill="#1e9afe" />
           <path d="M 0 0 h 100 v 100" fill="#ff2928" />
           <circle cx="48" cy="50" r="17.5" fill="#7b1ea2" stroke="white" stroke-width="5" />
           <path d="M 65.5 31 v 38" stroke-width="5" stroke="white" />
         </svg>
 
-        <div tw="ml-3 font-semibold">armaan</div>
+        <div tw="ml-5 font-semibold text-5xl">armaan</div>
       </header>
 
       <main tw="flex grow pb-3 flex-col justify-center">
         <div tw="flex">
           <div
-            tw="text-7xl pb-5 pr-5 font-bold rounded-md leading-1.25 text-transparent"
+            tw="text-8xl pb-5 pr-5 font-bold rounded-md leading-1.25 text-transparent"
             style={{
               backgroundImage: "linear-gradient(319deg, #663dff 0%, #aa00ff 37%, #cc4499 100%)",
               backgroundClip: "text",
@@ -44,7 +44,7 @@ export async function generateOgImage({ title, subtitle }: GenerateOgImage) {
           </div>
         </div>
 
-        <div tw="mt-5 flex text-3xl text-gray-500">{subtitle}</div>
+        <div tw="mt-5 flex text-5xl text-gray-500">{subtitle}</div>
       </main>
     </div>
   );
