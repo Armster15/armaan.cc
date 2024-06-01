e37c41f65f9f3428945550897c1a5da49667e4d0
Armaan A <armaanaggarwal@gmail.com>
2024-06-01 16:54:52 -0700

    always use light mode theme in code snippets

--- astro.config.ts
+++ astro.config.ts
@@ -22,6 +22,8 @@ export default defineConfig({
           frameBoxShadowCssValue: "",
         },
       },
+      themes: ["github-light"],
+      useDarkModeMediaQuery: false,
     }),
     mdx(),
     sitemap({
