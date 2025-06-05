4a8b87e6427f7e5ba05638644b4d7e4cc4916d3e
Armaan Aggarwal <armaanaggarwal@gmail.com>
2025-06-04 23:19:28 -0700

    fix fonts not working

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      fontFamily: { sans: ["Inter Variable", ...defaultTheme.fontFamily.sans] },
+      fontFamily: { sans: ["Satoshi Variable", ...defaultTheme.fontFamily.sans] },
       screens: {
         xs: "335px",
       },
