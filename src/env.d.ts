f32e3399fba4388b38b37f570a99b7c102e73514
Armaan Aggarwal <49625265+Armster15@users.noreply.github.com>
2025-09-23 00:12:28 -0700

    🪓 OG Image (#2)
    * base axe og image
    
    * remove description
    
    * add default og image
    
    * fix twitter card
    
    * dont hash casual pfp img
    
    * remove old desc

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,15 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-import "react";
-
-declare module "react" {
-  interface HTMLAttributes<T> extends DOMAttributes<T> {
-    /** ⚠️ Only for use with Satori */
-    tw?: string;
-  }
-  interface SVGProps<T> extends SVGProps<SVGSVGElement> {
-    /** ⚠️ Only for use with Satori */
-    tw?: string;
-  }
-}
