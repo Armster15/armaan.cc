7af9827038c1fdc1d9d153dcc07aed8f19111e40
Armaan Aggarwal <armaanaggarwal@gmail.com>
2025-09-23 00:18:20 -0700

    remove raw fonts vite plugin used for og image generation

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,8 @@
-import type { Plugin } from "vite";
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import expressiveCode from "astro-expressive-code";
-import { readFileSync } from "node:fs";
 
 // https://astro.build/config
 export default defineConfig({
@@ -36,23 +34,4 @@ export default defineConfig({
       lastmod: new Date(),
     }),
   ],
-  vite: {
-    plugins: [rawFonts([".ttf", ".woff"])],
-  },
 });
-
-// vite plugin to import fonts
-function rawFonts(ext: string[]) {
-  return {
-    name: "vite-plugin-raw-fonts",
-    transform(_, id) {
-      if (ext.some((e) => id.endsWith(e))) {
-        const buffer = readFileSync(id);
-        return {
-          code: `export default ${JSON.stringify(buffer)}`,
-          map: null,
-        };
-      }
-    },
-  } satisfies Plugin;
-}
