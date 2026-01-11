1d6e4ed32db82bed4ce63050be322691c8202628
Armaan Aggarwal <49625265+Armster15@users.noreply.github.com>
2026-01-10 19:26:41 -0800

    Add Prettier plugin to sort imports (#8)

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -2,6 +2,7 @@
 module.exports = {
   printWidth: 100,
   plugins: [
+    require.resolve("@ianvs/prettier-plugin-sort-imports"),
     require.resolve("prettier-plugin-astro"),
     require.resolve("prettier-plugin-tailwindcss"),
   ],
@@ -14,4 +15,39 @@ module.exports = {
     },
   ],
   tailwindStylesheet: "./src/styles/globals.css",
+  importOrder: [
+    // astro, builtin, third-party
+    "^astro($|:|/)",
+    "^@astrojs/",
+    "",
+
+    // third party modules, nodejs
+    "<BUILTIN_MODULES>",
+    "<THIRD_PARTY_MODULES>",
+    "",
+
+    // .astro files
+    "^.+[.]astro$",
+    "",
+
+    // $/ and relative imports (script files only)
+    "^\\$/.+[.](ts|tsx|js|jsx)$",
+    "^\\$/[^.]+$",
+    "^\\$/.+/[^.]+$",
+    "^[.]{1,2}/.+[.](ts|tsx|js|jsx)$",
+    "^[.]{1,2}/[^.]+$",
+    "^[.]{1,2}/.+/[^.]+$",
+    "",
+
+    // images, videos, fonts, icons, other assets
+    // (excludes css)
+    "^.+[.](?!module[.]css$)(?!css$|scss$|sass$|less$|styl$)[^/]+$",
+    "",
+    "",
+
+    // css
+    "^.+[.]module[.]css$",
+    "^.+[.](css|scss|sass|less|styl)$",
+    "",
+  ],
 };
