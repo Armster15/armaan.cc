a24b8349fe21a59989bc658da4531e6d02b51593
Armaan Aggarwal <armaanaggarwal@gmail.com>
2026-01-10 19:24:49 -0800

    Add Prettier plugin to sort imports

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+
+import tailwindcss from "@tailwindcss/vite";
 import expressiveCode from "astro-expressive-code";
 import icon from "astro-icon";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
